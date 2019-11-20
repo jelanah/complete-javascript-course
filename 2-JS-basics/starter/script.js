db475b46883874b4999d8f7aaf52511416dd7ca2
Jelanah Cheatham <jelanahcheatham@Jelanahs-MacBook-Pro.local>
2019-11-20 08:37:02 -0600

    Test Update

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,78 @@
+/************************************
+ * Variables and data types
+ */
+
+ /*
+console.log('Hello World!!!');
+
+var firstName = 'John';
+var age = 28;
+console.log(firstName);
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+*/
+
+
+/************************************
+ * Variable mutation and type coercion
+ */
+
+ /*
+var firstName = 'John';
+var age = 28; 
+// JavaScript automatically turns number into string
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job +
+'. Is he maried? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver'
+
+// Wont load page until user presses okay
+alert(firstName + ' is a ' + age + ' year old ' + job +
+'. Is he maried? ' + isMarried);
+
+// Prompts for data entry
+lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
+ */
+
+
+ /*****************************************
+  * Basic operators
+  */
+
+// Math operators
+var year, ageJohn, ageMark;
+now = 2019;
+ageJohn = now - 28;
+ageMark = now - 33;
+
+console.log(ageJohn);
+
+console.log(now / 10);
+
+// Logical operators
+var johnOlder = ageJoh ageMark;
+console.log(johnOlder);
+
+// Typeof operator
+
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+var x;
+console.log(typeof x);
+console.log(johnOlder);
+
+
